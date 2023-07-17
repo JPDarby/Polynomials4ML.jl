919d9c4e7ae268a8a9c4073c72a9059603a0a259
JPDarby <jamesdarby@live.co.uk>
2023-07-17 15:33:54 +0100

    whitespace fix

--- src/Polynomials4ML.jl
+++ src/Polynomials4ML.jl
@@ -1,79 +1,79 @@
 module Polynomials4ML
 
 # -------------- Import ObjectPools stuff ---------------
-using ObjectPools: acquire!, release!,
+using ObjectPools: acquire!, release!, 
                    FlexArray, FlexArrayCache, TSafe, ArrayPool
 
 
-# -------------- import ACEbase stuff
+# -------------- import ACEbase stuff 
 
 import ACEbase
-import ACEbase: evaluate, evaluate_d, evaluate_ed, evaluate_dd, evaluate_ed2,
+import ACEbase: evaluate, evaluate_d, evaluate_ed, evaluate_dd, evaluate_ed2, 
                 evaluate!, evaluate_d!, evaluate_ed!, evaluate_ed2!
 import ACEbase.FIO: read_dict, write_dict
 
-function natural_indices end   # could rename this get_spec or similar ...
+function natural_indices end   # could rename this get_spec or similar ... 
 function index end
 function orthpolybasis end
-function degree end
+function degree end 
 
 
-# some stuff to allow bases to overload some lux functionality ...
-# how much of this should go into ACEbase?
-function _valtype end
-function lux end
-function _init_luxparams end
-function _init_luxstate end
+# some stuff to allow bases to overload some lux functionality ... 
+# how much of this should go into ACEbase? 
+function _valtype end 
+function lux end 
+function _init_luxparams end 
+function _init_luxstate end 
 
-export natural_indices,
-       index,
-       evaluate,
-       evaluate_d,
-       evaluate_dd,
-       evaluate_ed,
-       evaluate_ed2,
-       evaluate!,
-       evaluate_ed!,
-       evaluate_ed2!,
-       orthpolybasis,
-       degree
+export natural_indices, 
+       index, 
+       evaluate, 
+       evaluate_d, 
+       evaluate_dd, 
+       evaluate_ed, 
+       evaluate_ed2, 
+       evaluate!, 
+       evaluate_ed!, 
+       evaluate_ed2!, 
+       orthpolybasis, 
+       degree 
 
 
 
-# generic fallbacks for a lot of wrapper kind of functionality
+# generic fallbacks for a lot of wrapper kind of functionality 
 include("interface.jl")
 
-# polynomials
+# polynomials 
 include("orthopolybasis.jl")
 include("discreteweights.jl")
 include("jacobiweights.jl")
 include("monomials.jl")
 
-# 2d harmonics / trigonometric polynomials
+# 2d harmonics / trigonometric polynomials 
 include("trig.jl")
 include("rtrig.jl")
 include("chebbasis.jl")
 
-# 3d harmonics
+# 3d harmonics 
 include("sphericalharmonics/sphericalharmonics.jl")
 
-# quantum chemistry
+# quantum chemistry 
 include("atomicorbitalsradials/atomicorbitalsradials.jl")
 
 # generating product bases (generalisation of tensor products)
 include("staticprod.jl")
 include("sparseproduct.jl")
 
-# generic machinery for wrapping poly4ml bases into lux layers
+# generic machinery for wrapping poly4ml bases into lux layers 
 include("lux.jl")
 
 # basis components to implement cluster expansion methods
 include("ace/ace.jl")
 
-# some nice utility functions to generate basis sets and other things
+# some nice utility functions to generate basis sets and other things  
 include("utils/utils.jl")
 
-# submodule with some useful utilities for writing unit tests
+# submodule with some useful utilities for writing unit tests 
 include("testing.jl")
 
 #laplacian eigengfunctions
